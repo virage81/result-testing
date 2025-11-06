8bfe94937495dd002d672bba3574e7aebcc545e8
virage81 <karimovminds@gmail.com>
2025-11-06 23:45:16 +0400

    feat: update JestProvider to use preloaded state

--- spec/integration/NewTaskBar.spec.tsx
+++ spec/integration/NewTaskBar.spec.tsx
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import { NewTaskBar } from "src/modules/NewTaskBar";
-import ue from "@testing-library/user-event";
-import { JestStoreProvider } from "../utils/JestStoreProvider";
-import * as taskSliceModule from "src/store/taskSlice";
+import { screen } from '@testing-library/react';
+import ue from '@testing-library/user-event';
+import { NewTaskBar } from 'src/modules/NewTaskBar';
+import * as taskSliceModule from 'src/store/taskSlice';
+import { JestStoreProvider } from '../utils/JestStoreProvider';
 
 // 1. Название задачи не должно быть больше 32 символов
 // 2. Название задачи не может быть пустым
@@ -10,45 +10,39 @@ import * as taskSliceModule from "src/store/taskSlice";
 // 4. Список не может содержать больше 10 невыполненных задач
 
 const userEvent = ue.setup({
-  advanceTimers: jest.advanceTimersByTime,
+	advanceTimers: jest.advanceTimersByTime,
 });
 
 it.each([
-  ["Длинный неподходящий заголовок тут", true, true],
-  ["Хороший заголовок", false, false],
-  ["", true, false],
-])("Создание задачи", async (text, isDisabled, isHinted) => {
-  render(<NewTaskBar />, {
-    wrapper: JestStoreProvider,
-  });
+	['Длинный неподходящий заголовок тут', true, true],
+	['Хороший заголовок', false, false],
+	['', true, false],
+])('Создание задачи', async (text, isDisabled, isHinted) => {
+	JestStoreProvider(<NewTaskBar />);
 
-  const inputEl = screen.getByRole("textbox");
-  const hintEl = screen.getByTestId("input-hint-text");
-  const addBtnEl = screen.getByRole("button");
+	const inputEl = screen.getByRole('textbox');
+	const hintEl = screen.getByTestId('input-hint-text');
+	const addBtnEl = screen.getByRole('button');
 
-  await userEvent.clear(inputEl);
+	await userEvent.clear(inputEl);
 
-  if (text) {
-    await userEvent.type(inputEl, text);
-  }
+	if (text) {
+		await userEvent.type(inputEl, text);
+	}
 
-  expect(inputEl).toHaveValue(text);
-  expect(addBtnEl.hasAttribute("disabled")).toBe(isDisabled);
-  expect(hintEl.innerHTML.length > 0).toBe(isHinted);
+	expect(inputEl).toHaveValue(text);
+	expect(addBtnEl.hasAttribute('disabled')).toBe(isDisabled);
+	expect(hintEl.innerHTML.length > 0).toBe(isHinted);
 });
 
-it("Не больше 10 новых задач", async () => {
-  const spied = jest
-    .spyOn(taskSliceModule, "uncompleteCount")
-    .mockReturnValue(10);
+it('Не больше 10 новых задач', async () => {
+	const spied = jest.spyOn(taskSliceModule, 'incompleteCount').mockReturnValue(10);
 
-  render(<NewTaskBar />, {
-    wrapper: JestStoreProvider,
-  });
+	JestStoreProvider(<NewTaskBar />);
 
-  const inputEl = screen.getByRole("textbox");
-  const addBtnEl = screen.getByRole("button");
+	const inputEl = screen.getByRole('textbox');
+	const addBtnEl = screen.getByRole('button');
 
-  expect(inputEl).toBeDisabled();
-  expect(addBtnEl).toBeDisabled();
+	expect(inputEl).toBeDisabled();
+	expect(addBtnEl).toBeDisabled();
 });
--- src/store/taskSlice.ts
+++ src/store/taskSlice.ts
@@ -1,6 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from "@reduxjs/toolkit";
-import { RootState } from "./configureStore";
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from './configureStore';
 
 export interface taskListState {
   list: Task[];
@@ -9,29 +9,29 @@ export interface taskListState {
 
 const initialState: taskListState = {
   list: [],
-  notification: "",
+	notification: '',
 };
 
 export const taskListSlice = createSlice({
-  name: "taskList",
+	name: 'taskList',
   initialState,
   reducers: {
-    addTask: (state, action: PayloadAction<Task["header"]>) => {
+		addTask: (state, action: PayloadAction<Task['header']>) => {
       state.list.push({
         id: crypto.randomUUID(),
         header: action.payload,
         done: false,
       });
     },
-    completeTask: (state, action: PayloadAction<Task["id"]>) => {
-      const task = state.list.find((x) => x.id === action.payload);
+		completeTask: (state, action: PayloadAction<Task['id']>) => {
+			const task = state.list.find(x => x.id === action.payload);
 
       if (task) {
         task.done = true;
       }
     },
-    toggleTask: (state, action: PayloadAction<Task["id"]>) => {
-      const task = state.list.find((x) => x.id === action.payload);
+		toggleTask: (state, action: PayloadAction<Task['id']>) => {
+			const task = state.list.find(x => x.id === action.payload);
 
       if (task) {
         task.done = !task.done;
@@ -41,25 +41,19 @@ export const taskListSlice = createSlice({
         }
       }
     },
-    deleteTask: (state, action: PayloadAction<Task["id"]>) => {
-      state.list = state.list.filter((x) => x.id !== action.payload);
+		deleteTask: (state, action: PayloadAction<Task['id']>) => {
+			state.list = state.list.filter(x => x.id !== action.payload);
     },
-    setNotification: (state, action: PayloadAction<Task["header"]>) => {
+		setNotification: (state, action: PayloadAction<Task['header']>) => {
       state.notification = `Задача "${action.payload}" завершена`;
     },
-    clearNotification: (state) => {
-      state.notification = "";
+		clearNotification: state => {
+			state.notification = '';
     },
   },
 });
 
-export const {
-  addTask,
-  completeTask,
-  deleteTask,
-  toggleTask,
-  clearNotification,
-} = taskListSlice.actions;
+export const { addTask, completeTask, deleteTask, toggleTask, clearNotification } = taskListSlice.actions;
 
 export default taskListSlice.reducer;
 
@@ -67,11 +61,11 @@ export const tasksSelector = (state: RootState) => state.taskList.list;
 
 export const fullCount = (state: RootState) => state.taskList.list.length;
 
-export const completeCount = (state: RootState) =>
-  state.taskList.list.filter((x) => x.done).length;
+export const completeCount = (state: RootState) => state.taskList.list.filter(x => x.done).length;
 
-export const uncompleteCount = (state: RootState) =>
-  state.taskList.list.filter((x) => !x.done).length;
+export const incompleteCount = (state: RootState) => state.taskList.list.filter(x => !x.done).length;
+
+export const getNotification = (state: RootState) => state.taskList.notification;
 
 export const getNotification = (state: RootState) =>
   state.taskList.notification
