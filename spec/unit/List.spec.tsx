311fe906668366db4fa5b4615299549f89a90114
virage81 <karimovminds@gmail.com>
2025-11-07 17:47:45 +0400

    feat: add unit test for task list

--- spec/unit/List.spec.tsx
+++ spec/unit/List.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { List } from 'src/components/List';
 
-it('отображение списка задач', () => {
+test('отображение списка задач', () => {
 	const onDelete = jest.fn();
 	const onToggle = jest.fn();
 
@@ -34,4 +34,21 @@ it('отображение списка задач', () => {
 	expect(firstRender).toMatchDiffSnapshot(secondRender);
 });
 
-it('Список содержит не больше 10 невыполненных задач', () => {});
+test.only('Список содержит не больше 10 невыполненных задач', () => {
+	const onDelete = jest.fn();
+	const onToggle = jest.fn();
+
+	const baseItemNames = ['Купить хлеб', 'Купить молоко', 'Выгулять собаку'];
+	const items = Array.from<Task>({ length: 11 }).map((item, i) => {
+		return {
+			id: String(i + 1),
+			header: `${baseItemNames[i % 3]}${i >= 3 ? ` #${Math.floor(i / 3) + 1}` : ''}`,
+			done: false,
+		};
+	});
+
+	const { getByLabelText } = render(<List items={items} onDelete={onDelete} onToggle={onToggle} />);
+	const listNotificationEl = getByLabelText('list-notification');
+
+	expect(listNotificationEl).not.toHaveTextContent('');
+});
