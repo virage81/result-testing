a089f6cd7ee40c1a3e5b881824841673074aafa5
virage81 <karimovminds@gmail.com>
2025-11-07 18:20:03 +0400

    feat: add unit tests for task item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { DeleteButton } from './DeleteButton';
 
 type Props = Task & {
@@ -6,11 +7,19 @@ type Props = Task & {
 };
 
 export const Item = (props: Props) => {
+	const { header } = props;
+	if (!header) return null;
+
+	const formattedHeader = useMemo(() => {
+		if (header.length > 32) return `${header.slice(0, 29)}...`;
+		return header;
+	}, [header]);
+
 	return (
 		<li className='item-wrapper'>
 			<input type='checkbox' id={props.id} defaultChecked={props.done} onChange={() => props.onToggle(props.id)} />
-			<label htmlFor={props.id} onClick={() => props.onToggle(props.id)}>
-				{props.done ? <s>{props.header}</s> : props.header}
+			<label aria-label='task-name' htmlFor={props.id} onClick={() => props.onToggle(props.id)}>
+				{props.done ? <s>{formattedHeader}</s> : formattedHeader}
 			</label>
 			<DeleteButton disabled={!props.done} onClick={() => props.onDelete(props.id)} />
 		</li>
